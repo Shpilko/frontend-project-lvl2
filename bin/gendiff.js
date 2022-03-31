d034ad1cde410de4ab8b1a74d5787bb0169c4009
Evgeniy Shpilco <callelia@yandex.ru>
2022-04-01 00:04:11 +0300

    add action to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -10,5 +11,8 @@ program
   .version('0.8.0')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
