995eddd1573f504c900ff473c56d37ab41010fc8
Evgeniy Shpilco <callelia@yandex.ru>
2022-04-01 19:35:31 +0300

    add logic for yml diff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import { readFileSync } from 'fs';
 
+import yaml from 'js-yaml';
+
+const getFiles = (data1, data2) => {
+  if (data1.endsWith('.json') && data2.endsWith('.json')) {
+    const file1 = JSON.parse(readFileSync(data1, 'utf8'));
+    const file2 = JSON.parse(readFileSync(data2, 'utf8'));
+
+    return { file1, file2 };
+  }
+
+  if ((data1.endsWith('.yml') || data1.endsWith('.yaml')) && (data2.endsWith('.yml') || data2.endsWith('.yaml'))) {
+    const file1 = yaml.load(readFileSync(data1, 'utf8'));
+    const file2 = yaml.load(readFileSync(data2, 'utf8'));
+
+    return { file1, file2 };
+  }
+
+  return null;
+};
+
 export default (filepath1, filepath2) => {
-  const file1 = JSON.parse(readFileSync(filepath1, 'utf8'));
-  const file2 = JSON.parse(readFileSync(filepath2, 'utf8'));
+  const { file1, file2 } = getFiles(filepath1, filepath2);
 
   const keys1 = Object.keys(file1);
   const keys2 = Object.keys(file2);
