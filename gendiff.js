aed9c0defc604069201b3cfbd74f53faa484b3ed
Evgeniy Shpilco <callelia@yandex.ru>
2025-03-21 23:04:40 +0600

    add support for file paths and output format option

--- gendiff.js
+++ gendiff.js
@@ -2,17 +2,16 @@
 
 import { Command } from 'commander';
 
-// Создаём объект программы с описанием и версией
 const program = new Command();
 
 program
+    .version('0.0.1', '-V, --version', 'output the version number')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');  // Указываем версию, командный флаг --version будет работать по умолчанию
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .action((filepath1, filepath2, options) => {
+        console.log(genDiff(filepath1, filepath2, options.format));
+    });
 
-// Определение справки
-program.parse(process.argv);
-
-// Если не были переданы аргументы или нужен вывод справки, выводим справку
-if (process.argv.length <= 2) {
-    program.outputHelp(); // Выводит справочную информацию, если не переданы аргументы
-}
+program.parse();
