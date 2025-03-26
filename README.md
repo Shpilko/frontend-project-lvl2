c85fec28a82e051552bb30f18d315907bc9df169
Evgeniy Shpilco <callelia@yandex.ru>
2025-03-26 17:14:14 +0600

    Implemented a comparison of differences between two flat JSON files. Added asciinema.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Shpilko/frontend-project-lvl2/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Shpilko/frontend-project-lvl2/actions)
+
+[![asciicast](https://asciinema.org/a/iHocqOEynHiGKKEwHAg4nZ2MM.svg)](https://asciinema.org/a/iHocqOEynHiGKKEwHAg4nZ2MM)
--- bin/genDiff.js
+++ bin/genDiff.js
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander/esm.mjs';
 import parser from '../src/parser.js'
+import _ from 'lodash';
 
 const program = new Command();
 
@@ -11,9 +12,32 @@ program
     .option('-f, --format <type>', 'output format')
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2) => {
-        // console.log(parser(filepath1), parser(filepath2));
-        console.log(parser(filepath1));
-        console.log(parser(filepath2));
+        const file1 = parser(filepath1);
+        const file2 = parser(filepath2);
+
+        const keys1 = Object.keys(file1);
+        const keys2 = Object.keys(file2);
+        const keys = _.sortBy([...new Set([...keys1, ...keys2])]);
+
+        const result = keys.map((key) => {
+            if (keys1.includes(key) && !keys2.includes(key)) {
+                return `  - ${key}: ${file1[key]}`;
+            }
+            if (keys1.includes(key) && keys2.includes(key) && file1[key] === file2[key]) {
+                return `    ${key}: ${file1[key]}`;
+            }
+            if (keys1.includes(key) && keys2.includes(key) && file1[key] !== file2[key]) {
+                return `  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
+            }
+            if (!keys1.includes(key) && keys2.includes(key)) {
+                return `  + ${key}: ${file2[key]}`;
+            }
+
+            return null;
+        });
+
+        console.log(`{\n${result}\n}`.split(',').join('\n'));
+        return `{\n${result}\n}`.split(',').join('\n');
     });
 
 program.parse();
